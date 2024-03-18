2a53394b7e7c8c824cbd0a08d67fb8e81ac9e5d7
sasa0005 <sasa0005@student.monash.edu>
2024-03-18 18:35:00 +0800

    type fixed

--- test.py
+++ test.py
@@ -32,7 +32,7 @@ def test_psnr():
     gt_tensor_1 = torch.tensor([[[1.0, 2.0], [3.0, 4.0]]])
     predicted_tensor_1 = torch.tensor([[[1.5, 2.5], [2.5, 3.5]]])
     mse1 = metrics.mse_per_sample(predicted_tensor_1, gt_tensor_1)
-    psnr_value_1 = metrics.pnsr_per_sample(mse1)
+    psnr_value_1 = metrics.psnr_per_sample(mse1)
     expected_result_1 = torch.tensor([22.0403])  # Expected PSNR value for test case 1
     assert torch.allclose(psnr_value_1, torch.tensor(expected_result_1)), f"Test case 1 failed: PSNR value is incorrect: {psnr_value_1}"
 
@@ -40,7 +40,7 @@ def test_psnr():
     gt_tensor_2 = torch.tensor([[[1.0, 1.0], [1.0, 1.0]]])
     predicted_tensor_2 = torch.tensor([[[2.0, 2.0], [2.0, 2.0]]])
     mse2 = metrics.mse_per_sample(predicted_tensor_2, gt_tensor_2)
-    psnr_value_2 = metrics.pnsr_per_sample(mse2)
+    psnr_value_2 = metrics.psnr_per_sample(mse2)
     expected_result_2 = torch.tensor([0.0])  # Expected PSNR value for test case 2
     assert torch.allclose(psnr_value_2, torch.tensor(expected_result_2)), f"Test case 2 failed: PSNR value is incorrect: {psnr_value_2}"
 
@@ -48,7 +48,7 @@ def test_psnr():
     gt_tensor_3 = torch.tensor([[[1.0, 2.0, 3.0]]])
     predicted_tensor_3 = torch.tensor([[[3.0, 2.0, 1.0]]])
     mse3 = metrics.mse_per_sample(predicted_tensor_3, gt_tensor_3)
-    psnr_value_3 = metrics.pnsr_per_sample(mse5)
+    psnr_value_3 = metrics.psnr_per_sample(mse3)
     expected_result_3 = torch.tensor([0.0]) # Expected PSNR value for test case 3
     assert torch.allclose(psnr_value_3, torch.tensor(expected_result_3)), f"Test case 3 failed: PSNR value is incorrect: {psnr_value_3}"
 
