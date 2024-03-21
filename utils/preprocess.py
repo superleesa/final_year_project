490c1f3519297ee5a5c698e15b2c3f8c8c6cc4cb
Satoshi <6world4trigger@gmail.com>
2024-03-22 05:34:48 +0800

    Add Dataset class

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,16 +1,43 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
+from typing import List
 
-import os
+from PIL.Image import Image
+import numpy as np
+from torch.utils.data import Dataset
 import random
 from tqdm import tqdm
+import torch
 
+
+class SIEDataset(Dataset):
+    # load all images at once
+    # we use float32 for the image data type
+    def __init__(self, sand_dust_images: List[Image], ground_truth_images: List[Image], w_threshold: int, h_threshold: int):
+        assert len(sand_dust_images) == len(ground_truth_images), "the number of sand dust images and ground truth images must be the same"
+        self.sand_dust_images = sand_dust_images
+        self.ground_truth_images = ground_truth_images
+        self.w_threshold = w_threshold
+        self.h_threshold = h_threshold
+
+    def __len__(self):
+        return len(self.sand_dust_images)
+
+    def preprocess(self, image: Image) -> torch.Tensor:
+        image = resize_image(image, self.w_threshold, self.h_threshold)
+        image = np.asarray(image) / 255.0
+        image = np.transpose(image, axes=[2, 0, 1]).astype('float32')  # to C, H, W
+        image = torch.from_numpy(image).float()
+        return image
+
+    def __getitem__(self, idx) -> (torch.Tensor, torch.Tensor):
+        sand_dust_image = self.preprocess(self.sand_dust_images[idx])
+        ground_truth_image = self.preprocess(self.ground_truth_images[idx])
+
+        return sand_dust_image, ground_truth_image
 
 def crop_image(image, w_threshold, h_threshold):
     assert image.width >= w_threshold and image.height >= h_threshold, "to crop, image size must be bigger than or equal to the threshold values"
 
-    # choose top and right randomly -> bottom and left automallycally determined
+    # choose top and right randomly -> bottom and left automatically determined
     top = random.randint(0, image.height - h_threshold)  # inclusive
     left = random.randint(0, image.width - w_threshold)
 
@@ -55,3 +82,9 @@ def resize_images(images, w_threshold, h_threshold):
         resized_images.append(new_image)
 
     return resized_images
+
+def resize_image(image: Image, w_threshold: int, h_threshold: int) -> Image:
+    if is_image_smaller_than_threshold(image, w_threshold, h_threshold):
+        image = stretch_image(image, w_threshold, h_threshold)
+
+    return crop_image(image, w_threshold, h_threshold)
