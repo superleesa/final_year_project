ef5563e707993dfd3e93acc4d8b1aced815b9291
Satoshi <6world4trigger@gmail.com>
2024-04-03 10:52:05 +0800

    Add basic backend code

---
+++ app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, send_file
+import os
+
+from model import load_model, restore_and_save_one
+from utils import save_image
+
+app = Flask(__name__)
+
+# TODO: create appropriate config file
+app.config["is_mock"] = True
+app.config['upload_folder'] = '/images'
+app.config["checkpoint_dir"] = "/checkpoint"
+
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+@app.route("/restore-image", methods=["POST"])
+def restore_image():
+    filename = save_image()
+    if app.config["is_mock"]:
+        # load a sample image
+        return send_file(os.path.join(app.config["upload_folder"], "images", "sample_image.jpg"), mimetype='image/gif')
+    else:
+        # TODO: add caching
+        model = load_model(app.config["checkpoint_dir"])
+        restored_filename = restore_and_save_one(model, filename)
+        return send_file(restored_filename, mimetype='image/gif')
---
+++ app/utils.py
@@ -0,0 +1,10 @@
+import os
+from flask import request, current_app
+from werkzeug.utils import secure_filename
+
+
+def save_image() -> str:
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(current_app.config['upload_folder'], filename))
+    return filename
