8fe96c95f03a779293146d1142dc7f8c3f917588
sasa0005 <sasa0005@student.monash.edu>
2024-04-03 20:03:01 +0800

    add create_evaluation_dataset

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -14,11 +14,32 @@ NOISY_IMAGE_DIR_NAME = "noisy"
 GROUND_TRUTH_IMAGE_DIR_NAME = "ground_truth"
 CLEAR_IMAGE_DIR_NAME = "clear"
 
-class PairedDataset(Dataset):
+class PairedTrainDataset(Dataset):
     def __init__(self, sand_dust_images: List[np.ndarray],
                  ground_truth_images: List[np.ndarray],
-                 output_image_names: List[str],
                  transformer: CoupledCompose | v2.Compose) -> None:
+        self.sand_dust_images = sand_dust_images
+        self.ground_truth_images = ground_truth_images
+
+        self.transformer = transformer
+
+    def __len__(self) -> int:
+        return len(self.sand_dust_images)
+
+    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]:
+        if isinstance(self.transformer, CoupledCompose):
+            sand_dust_image, ground_truth_image = self.transformer(self.sand_dust_images[idx].copy(), self.ground_truth_images[idx].copy())
+        elif isinstance(self.transformer, v2.Compose):
+            # copy to ensure arrays are different across Datasets
+            sand_dust_image = self.transformer(self.sand_dust_images[idx].copy())
+            ground_truth_image = self.transformer(self.ground_truth_images[idx].copy())
+        return sand_dust_image, ground_truth_image
+
+class EvaluationDataset(Dataset):
+    def __init__(self, sand_dust_images: List[np.ndarray],
+                ground_truth_images: List[np.ndarray],
+                output_image_names: List[str],
+                transformer: v2.Compose) -> None:
         """
         output_image_names: should only be the name of the image without the extension and the path
         """
@@ -31,16 +52,12 @@ class PairedDataset(Dataset):
     def __len__(self) -> int:
         return len(self.sand_dust_images)
 
-    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        if isinstance(self.transformer, CoupledCompose):
-            sand_dust_image, ground_truth_image = self.transformer(self.sand_dust_images[idx].copy(), self.ground_truth_images[idx].copy())
-        elif isinstance(self.transformer, v2.Compose):
-            # copy to ensure arrays are different across Datasets
-            sand_dust_image = self.transformer(self.sand_dust_images[idx].copy())
-            ground_truth_image = self.transformer(self.ground_truth_images[idx].copy())
+    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor, str]:
+        sand_dust_image = self.transformer(self.sand_dust_images[idx].copy())
+        ground_truth_image = self.transformer(self.ground_truth_images[idx].copy())
         return sand_dust_image, ground_truth_image, self.output_image_names[idx]
 
-class UnpairedDataset:
+class UnpairedTrainDataset:
     """This class must be instantiated for each epoch to change pairs."""
     def __init__(self, sand_dust_images: List[np.ndarray],
                  clear_images: List[np.ndarray],
@@ -61,7 +78,6 @@ class UnpairedDataset:
         clear_image = self.transformer(self.clear_images[idx].copy())
         return sand_dust_image, clear_image
 
-
 def load_images_in_a_directory(directory_path: str) -> tuple[List[np.ndarray], List[str]]:
     images = []
     image_names = []
@@ -80,7 +96,37 @@ def load_images_in_a_directory(directory_path: str) -> tuple[List[np.ndarray], L
 def sort_image_by_filenames(images: List[np.ndarray], image_names: List[str]) -> List[np.ndarray]:
     return [image for image, _ in sorted(zip(images, image_names), key=lambda x: x[1])]
 
-def create_paired_datasets(dataset_dir: str, is_train=False, num_datasets: int = 1) -> List[PairedDataset]:
+def create_paired_datasets(dataset_dir: str, num_datasets: int = 1) -> List[PairedTrainDataset]:
+    """
+    dataset_dir: directory of the dataset (e.g. "Data/Synthetic_images/")
+    """
+    noisy_path = os.path.join(dataset_dir, NOISY_IMAGE_DIR_NAME)
+    noisy_images, noisy_image_names = load_images_in_a_directory(noisy_path)
+    noisy_images = sort_image_by_filenames(noisy_images, noisy_image_names)
+
+    gt_path = os.path.join(dataset_dir, GROUND_TRUTH_IMAGE_DIR_NAME)
+    gt_images, gt_image_names = load_images_in_a_directory(gt_path)
+    gt_images = [gt_image for gt_image, _ in sorted(zip(gt_images, gt_image_names), key=lambda x: x[1])]
+
+    return [PairedTrainDataset(noisy_images, gt_images, train_paired_transform) for _ in range(num_datasets)]
+
+def create_unpaired_datasets(dataset_dir: str, num_datasets: int = 1) -> List[UnpairedTrainDataset]:
+    """
+    dataset_dir: directory of the dataset (e.g. "Data/Synthetic_images/")
+    Do not use thsi funciton when evaluating
+    """
+    noisy_path = os.path.join(dataset_dir, NOISY_IMAGE_DIR_NAME)
+    noisy_images, noisy_image_names = load_images_in_a_directory(noisy_path)
+    noisy_images = sort_image_by_filenames(noisy_images, noisy_image_names)
+
+    clear_path = os.path.join(dataset_dir, CLEAR_IMAGE_DIR_NAME)
+    clear_images, clear_image_names = load_images_in_a_directory(clear_path)
+    clear_images = sort_image_by_filenames(clear_images, clear_image_names)
+
+    return [UnpairedTrainDataset(noisy_images, clear_images, train_unpaired_transform) for _ in range(num_datasets)]
+
+
+def create_evaluation_dataset(dataset_dir: str) -> EvaluationDataset:
     """
     dataset_dir: directory of the dataset (e.g. "Data/Synthetic_images/")
     """
@@ -94,20 +140,4 @@ def create_paired_datasets(dataset_dir: str, is_train=False, num_datasets: int =
 
     denoised_image_file_names = [f"{index}_denoised" for index in range(len(noisy_images))]
 
-    transformer = train_paired_transform if is_train else eval_transform
-    return [PairedDataset(noisy_images, gt_images, denoised_image_file_names, transformer) for _ in range(num_datasets)]
-
-def create_unpaired_datasets(dataset_dir: str, num_datasets: int = 1) -> List[UnpairedDataset]:
-    """
-    dataset_dir: directory of the dataset (e.g. "Data/Synthetic_images/")
-    Do not use thsi funciton when evaluating
-    """
-    noisy_path = os.path.join(dataset_dir, NOISY_IMAGE_DIR_NAME)
-    noisy_images, noisy_image_names = load_images_in_a_directory(noisy_path)
-    noisy_images = sort_image_by_filenames(noisy_images, noisy_image_names)
-
-    clear_path = os.path.join(dataset_dir, CLEAR_IMAGE_DIR_NAME)
-    clear_images, clear_image_names = load_images_in_a_directory(clear_path)
-    clear_images = sort_image_by_filenames(clear_images, clear_image_names)
-
-    return [UnpairedDataset(noisy_images, clear_images, train_unpaired_transform) for _ in range(num_datasets)]
+    return EvaluationDataset(noisy_images, gt_images, denoised_image_file_names, eval_transform)
