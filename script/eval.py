7975e3efe52b084eec2beb5c86ac727570c68aca
Satoshi <6world4trigger@gmail.com>
2024-04-03 18:43:57 +0800

    Fix evaluation script

--- script/eval.py
+++ script/eval.py
@@ -1,21 +1,32 @@
 from fire import Fire
-from metrics import mse_per_sample, psnr_per_sample, ssim_per_sample
+from torch.utils.data import DataLoader
+import pandas as pd
+import os
+from datetime import datetime
 
-def evaluation_script(y_test, trained_model):
-    y_pred = trained_model(eval_data)
-    mse_value = mse_per_sample(y_pred, y_test)
-    psnr_value = psnr_per_sample(mse_value)
-    ssim_value = ssim_per_sample(y_pred, y_test)
+from metrics import mse_per_sample, psnr_per_sample, ssim_per_sample
+from validate import validate
+from utils.preprocess import create_paired_datasets
+
+
+def evaluation_script(images_dir, checkpoint_dir: str, save_dir: str, save_images_type: str) -> None:
+    # create unique save directory
+    formatted_current_datetime = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    save_dir = os.path.join(save_dir, f'run_{formatted_current_datetime}')
+    os.makedirs(save_dir, exist_ok=True)
+
+    datasets = create_paired_datasets(images_dir)
+    dataset = datasets[0]
+    dataloader = DataLoader(dataset, batch_size=4)
+    psnr_per_sample, ssim_per_sample = validate(dataloader, save_dir, checkpoint_dir, save_images=save_images_type)
     
     # Create DataFrame
-    metrics_df = pd.DataFrame({
-        'MSE': [mse_value],
-        'PSNR': [psnr_value],
-        'SSIM': [ssim_value]
+    df_metrics = pd.DataFrame({
+        'psnr': psnr_per_sample,
+        'ssim': ssim_per_sample
     })
+    df_metrics.to_csv(os.path.join(save_dir, 'evaluation_results.csv'), index=False)
 
-    # Save DataFrame as CSV
-    metrics_df.to_csv('evaluation_results.csv', index=False)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     Fire(evaluation_script)
