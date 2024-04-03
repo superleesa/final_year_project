e3347bbf1035f7ad057212e07641e640746cdadc
Satoshi <6world4trigger@gmail.com>
2024-04-03 19:01:43 +0800

    Fix paired_train script

--- script/paired_train.py
+++ script/paired_train.py
@@ -1,14 +1,20 @@
-from utils.preprocess import create_paired_dataset
-from train.sft import train as train_sft
 from fire import Fire
+from matplitlib.pyplot as plt
+import pandas as pd
+
+from utils.preprocess import create_paired_dataset
+from train.sft import train
+from utils import create_unique_save_dir
 
 # For paired images training in TOENet
-def paired_train_script(input_image_dir, save_dir, checkpoint_dir=None):
+def paired_train_script(images_dir: str, checkpoint_dir: str, save_dir: str) -> None:
+    save_dir = create_unique_save_dir(save_dir)
+
     # Create paired dataset from input images directory
-    dataset = create_paired_dataset(input_image_dir)
+    datasets = create_paired_datasets(images_dir, is_train=True)
     
     # Train TOENet model using supervised fine-tuning
-    trained_sft_model, sft_loss_records = train_sft(dataset, checkpoint_dir, save_dir)  # Checkpoints will be saved inside `save_dir`
+    _, sft_loss_records = train(datasets, checkpoint_dir, save_dir)  # Checkpoints will be saved inside `save_dir`
 
     # Save loss_records in csv
     paired_loss_df = pd.DataFrame(sft_loss_records, columns=["loss"])
@@ -21,6 +27,7 @@ def paired_train_script(input_image_dir, save_dir, checkpoint_dir=None):
     plt.title("Paired Image Training Loss Curve")
     plt.savefig(f"{save_dir}/paired_loss_curve.png")
     plt.close()
+
 
 if __name__ = "__main__":
     Fire(paired_train_script)
