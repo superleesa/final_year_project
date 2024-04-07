Fix bug in val loss computation

train/sft/toenet_base/train.py
@@ -27,6 +27,7 @@ def validate_loop(
         loss_gamma2: float,
         color_loss_criterion: nn.CosineSimilarity,
         l2_criterion: nn.MSELoss,
+        batch_size: int
 ) -> float:
 
     model.eval()
@@ -42,7 +43,7 @@ def validate_loop(
             total_loss = loss_gamma1 * l2 + loss_gamma2 * color_loss
 
             # FIXME: technically this is incorrect because the last batch might have a different size
-            loss_mean += total_loss.cpu().item() * (1/(len(val_dataloader)))
+            loss_mean += total_loss.cpu().item() * (batch_size/(len(val_dataloader)))
     return loss_mean
 
 def train_loop(train_datasets: list[PairedDataset], val_datasets: list[PairedDataset], checkpoint_dir: str, save_dir: str) -> tuple[TOENet, list[int], list[int], list[int]]:
@@ -101,6 +102,7 @@ def train_loop(train_datasets: list[PairedDataset], val_datasets: list[PairedDat
                     loss_gamma2,
                     color_loss_criterion,
                     l2_criterion,
+                    config["batch_size"],
                 )
 
                 val_loss_records.append(val_loss)

train/uft/train.py
@@ -83,6 +83,7 @@ def validate_loop(
         discriminator_criterion: nn.BCELoss,
         clip_min: float | None,
         clip_max: float | None,
+        batch_size: int,
 ) -> tuple[float, float]:
 
     denoiser.eval()
@@ -126,7 +127,7 @@ def validate_loop(
                 normal_images_predicted_labels,
             )
 
-            discriminator_loss_mean += discriminator_loss.cpu().item() * (1 / (len(val_dataloader)))
+            discriminator_loss_mean += discriminator_loss.cpu().item() * (batch_size / (len(val_dataloader)))
 
     return denoiser_loss_mean, discriminator_loss_mean
 
@@ -234,6 +235,7 @@ def train_loop(train_datasets: list[UnpairedDataset], val_datasets: list[Unpaire
                     discriminator_adversarial_criterion,
                     clip_min,
                     clip_max,
+                    config["batch_size"]
                 )
                 val_denoiser_loss_records.append(vaL_denoiser_loss)
                 val_discriminator_loss_records.append(val_discriminator_loss)