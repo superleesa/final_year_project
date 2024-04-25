118cb66dd3936e9927198d545d17b4bae6d7067a
Satoshi <6world4trigger@gmail.com>
2024-04-25 20:26:03 +0800

    Port create_dir_with_hyperparam_name to utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -38,6 +38,13 @@ def load_checkpoint(checkpoint_path: str, is_gpu: bool):
     return model
 
 
-def update_key_if_not_none(params: dict, key: str, new_value: Any) -> None:
+def update_key_if_new_value_is_not_none(params: dict, key: str, new_value: Any) -> None:
     if new_value is not None:
         params[key] = new_value
+
+def create_dir_with_hyperparam_name(base_save_dir: str, denoiser_loss_b1: float, denoiser_loss_b2: float) -> str:
+    folder_name = f"b1={denoiser_loss_b1}_b2={denoiser_loss_b2}"
+    path = Path(base_save_dir) / folder_name
+    path.mkdir(parents=True, exist_ok=True)
+    return str(path)
+
