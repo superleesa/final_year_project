b202d6f11a1eac3d138300fb4da5400d4150a7da
Winnie Ooi <ooiyt6512@gmail.com>
2024-05-05 02:12:32 +0800

    - added optuna to requirements.txt - run optuna for best hyperparameter and best loss (workable)

--- train/uft/tuning.py
+++ train/uft/tuning.py
@@ -13,21 +13,21 @@ from utils.preprocess import (
     create_train_and_validation_unpaired_datasets,
     create_evaluation_dataset,
 )
-from utils.eval_plots import (
-    plot_and_save_metrics,
-    plot_and_save_metric_distribution,
-    calc_and_save_average_metrics,
-    save_aggregated_data,
-)
+# from utils.eval_plots import (
+#     plot_and_save_metrics,
+#     plot_and_save_metric_distribution,
+#     calc_and_save_average_metrics,
+#     save_aggregated_data,
+# )
 from utils.utils import (
     create_unique_save_dir,
     update_key_if_new_value_is_not_none,
     create_dir_with_hyperparam_name,
 )
-from utils.train_plots import (
-    save_denoiser_and_discriminator_loss_records_in_csv,
-    plot_train_and_val_loss_curves,
-)
+# from utils.train_plots import (
+#     save_denoiser_and_discriminator_loss_records_in_csv,
+#     plot_train_and_val_loss_curves,
+# )
 
 
 DENOISER_LOSS_B1_OPTIONS = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
@@ -61,44 +61,43 @@ def load_uft_params_from_yml(config_path: str | Path) -> dict:
         "denoiser_adversarial_loss_clip_min": config.get("clip_min"),
         "denoiser_adversarial_loss_clip_max": config.get("clip_max"),
         "save_images_type": config.get("save_images_type") or "all",
+        "checkpoint_path": config.get("checkpoint_path"),
+        "images_dir": config.get("images_dir"),
+        "save_dir": config.get("save_dir")
     }
 
 def get_params(trial):
     # Generate the optimizers.
     params = {
-            'num_epochs':  trial.suggest_int('num_epochs', 5, 50), 
-            'denoiser_adam_lr ': trial.suggest_float('denoiser_adam_lr', 1e-6, 1e-2, log = True), 
+            'denoiser_adam_lr': trial.suggest_float('denoiser_adam_lr', 1e-6, 1e-2, log = True), 
             'discriminator_adam_lr': trial.suggest_float('discriminator_adam_lr', 1e-6, 1e-2, log = True), 
-            "step_lr_step_size" : trial.suggest_float('step_lr_step_size', 10, 50), 
-            "step_lr_gamma" : trial.suggest_float('step_lr_gamma', 0.1, 0.5),
-            'batch_size': trial.suggest_int('batch_size', 4, 64, step = 4),
+            'denoiser_loss_b1' : trial.suggest_int('denoiser_loss_b1', 0.1, 0.9),
+            'denoiser_loss_b2' : trial.suggest_float('denoiser_loss_b2',0.1, 0.9),
+            'batch_size': trial.suggest_int('batch_size', 4, 24, step = 4),
+            'num_epochs':  trial.suggest_int('num_epochs', 5, 15), 
             'print_loss_interval': trial.suggest_int('print_loss_interval', 50, 500),
-            "calc_eval_loss_interval" : trial.suggest_int("calc_eval_loss_interval", 50, 500),
-            "denoiser_loss_b1" : trial.suggest_int("denoiser_loss_b1", 0.1, 0.9),
-            "denoiser_loss_b2" : trial.suggest_float('denoiser_loss_b2',0.1, 0.9),
-            "train_ratio" : 0.7,
-            'checkpoint_path': "C:\Users\super\Documents\1.School\FYP\fyp_project\src\toenet\checkpoint\checkpoint.pth.tar",
-            'images_dir': "C:\Users\super\Documents\1.School\FYP\fyp_project\Data\input\unpaired",
-            'save_dir': "C:\Users\super\Documents\1.School\FYP\fyp_project\Data\output"
+            'denoiser_adversarial_loss_clip_min': trial.suggest_float('denoiser_adversarial_loss_clip_min', 0.0, 1.0),
+            'denoiser_adversarial_loss_clip_max': trial.suggest_float('denoiser_adversarial_loss_clip_max', 0.0, 1.0),
+            'calc_eval_loss_interval': trial.suggest_int("calc_eval_loss_interval", 50, 500),
     }
 
     return params
 
 def objective (trial):
-    images_dir_from_config = get_params(trial).pop("images_dir")
-    images_dir = images_dir or images_dir_from_config
+    images_dir_from_config = r"/home/student/Documents/mds12/winnie/final_year_project/Data/unpaired"
+    images_dir =  images_dir_from_config
     num_epochs = get_params(trial)["num_epochs"]
-    train_ratio = get_params(trial)["train_ratio"] or 0.8
-    checkpoint_path = get_params(trial).pop("checkpoint_path")
+    train_ratio = 0.7
+    checkpoint_path = r"/home/student/Documents/mds12/winnie/final_year_project/src/toenet/checkpoint/checkpoint.pth.tar"
 
-    base_save_dir = get_params(trial)["save_dir"]
+    base_save_dir = r"/home/student/Documents/mds12/winnie/final_year_project/Data/output"
     base_save_dir = create_unique_save_dir(base_save_dir)
 
     current_run_save_dir = create_dir_with_hyperparam_name(
             base_save_dir, get_params(trial)["denoiser_loss_b1"], get_params(trial)["denoiser_loss_b2"]
     )
 
-    get_params(trial)["save_dir"] = current_run_save_dir
+    base_save_dir = current_run_save_dir
 
     train_datasets, val_datasets = create_train_and_validation_unpaired_datasets(
             images_dir, num_epochs, train_ratio=train_ratio
@@ -114,79 +113,22 @@ def objective (trial):
                 val_denoiser_loss_records,
                 val_discriminator_loss_records,
         ),
-    )= train_loop(train_datasets, val_datasets, **get_params(trial))
-    all_runs_discriminator_loss_records.append(val_denoiser_loss_records)
-    all_runs_denoiser_loss_records.append(val_discriminator_loss_records)
+    )= train_loop(train_datasets, val_datasets, checkpoint_path, current_run_save_dir, **get_params(trial))
 
-    # save train and val loss values for this run
-    save_denoiser_and_discriminator_loss_records_in_csv(
-        list(range(0, len(denoiser_loss_records))),
-        denoiser_loss_records,
-        discriminator_loss_records,
-        get_params(trial)["save_dir"],
-    )
-    save_denoiser_and_discriminator_loss_records_in_csv(
-        val_loss_computed_indices,
-        val_denoiser_loss_records,
-        val_discriminator_loss_records,
-        get_params(trial)["save_dir"],
-    )
-
-    # save denoiser and discriminator loss curves
-    plot_train_and_val_loss_curves(
-        list(range(0, len(denoiser_loss_records))),
-        val_loss_computed_indices,
-        denoiser_loss_records,
-        val_denoiser_loss_records,
-        "Unpaired Image Training Denoiser Loss Curve",
-        get_params(trial)["save_dir"],
-    )
-    plot_train_and_val_loss_curves(
-        list(range(0, len(discriminator_loss_records))),
-        val_loss_computed_indices,
-        discriminator_loss_records,
-        val_discriminator_loss_records,
-        "Unpaired Image Training Discriminator Loss Curve",
-        get_params(trial)["save_dir"],
-    )
-
-    # evaluation
-    dataset = create_evaluation_dataset(images_dir)
-    dataloader = DataLoader(dataset, batch_size=get_params(trial)["batch_size"])
-    psnr_per_sample, ssim_per_sample = evaluate(
-        dataloader,
-        get_params(trial)["save_dir"],
-        checkpoint_path,
-        save_images=get_params(trial)["save_images_type"],
-    )
-    # todo: there should be a function that only handles creation of metrics dataframe
-    # the function below current save created plot and return df
-    df_metric = plot_and_save_metrics(
-        {"psnr": psnr_per_sample, "ssim": ssim_per_sample}, get_params(trial)["save_dir"]
-    )
-    plot_and_save_metric_distribution(df_metric["psnr"], "PSNR", get_params(trial)["save_dir"])
-    plot_and_save_metric_distribution(df_metric["ssim"], "SSIM", get_params(trial)["save_dir"])
-    calc_and_save_average_metrics(
-        df_metric["psnr"], df_metric["ssim"], get_params(trial)["save_dir"]
-    )
-    all_runs_avg_psnr_records.append(df_metric["psnr"].mean().item())
-    all_runs_avg_ssim_records.append(df_metric["ssim"].mean().item())
-
-    save_aggregated_data(
-        all_runs_discriminator_loss_records,
-        all_runs_denoiser_loss_records,
-        all_runs_avg_psnr_records,
-        all_runs_avg_ssim_records,
-        os.path.join(base_save_dir, "aggregated_results"),
-    )
-
-    return (val_denoiser_loss_records, val_discriminator_loss_records)//2
+    # Calculate the average validation loss for denoiser and discriminator
+    avg_denoiser_loss = sum(val_denoiser_loss_records) / len(val_denoiser_loss_records)
+    avg_discriminator_loss = sum(val_discriminator_loss_records) / len(val_discriminator_loss_records)
+    
+    return (avg_denoiser_loss + avg_discriminator_loss)
 
 def run_tuning():
     study = optuna.create_study(direction='minimize', study_name="uft-tuning")
+    study.optimize(objective, n_trials=10)
     print("Best hyperparameters:", study.best_params)
     print("Best value:", study.best_value)
 
 
-    
+if __name__ == "__main__":
+    run_tuning()
+   
         
